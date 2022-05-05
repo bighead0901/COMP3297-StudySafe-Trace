f22c0991b9b4cb9e6960e3d144a72771a67cbcd3
BuildTools <unconfigured@null.spigotmc.org>
2022-05-05 11:30:14 +0800

    test

--- hello/views.py
+++ hello/views.py
@@ -5,22 +5,55 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
+    user = "No input"
+    date = "No input"
+    if request.method == "POST":
+        user = request.POST.get('uid')
+        if user == "3025704501":
+            date = "2022-05-05"
+        else:
+            date = "No record"
+        
     return render(request, "base.html", {
-        "venues": ["this subject", "on99"],
-        "subject": "3025704501",
-        "date": "2022-05-05",
+        "venues": ["dllm", "on99"],
+        "subject": user,
+        "date": date,
     })
 
 def venues(request):
-    return render(request, "venues.html", {
-        "venues": ["dllm", "on99"],
-        "subject": "3025704501",
-        "date": "2022-05-05",
-    })
+    if request.method == "POST":
+        if request.POST.get('venues'):
+            user = request.POST.get('uid')
+            if user == "3025704501":
+                date = "2022-05-05"
+            else:
+                date = "No record"
+            return render(request, "venues.html", {
+                "venues": ["dllm", "on99"],
+                "subject": user,
+                "date": date,
+            })
+        else:
+            return render(request, "base.html", {
+                "subject": "No input",
+                "date": "No input",
+            })
 
 def contacts(request):
-    return render(request, "contacts.html", {
-        "venues": ["dllm", "on99"],
-        "subject": "3025704501",
-        "date": "2022-05-05",
-    })
+    if request.method == "POST":
+        if request.POST.get('contacts'):
+            user = request.POST.get('uid')
+            if user == "3025704501":
+                date = "2022-05-05"
+            else:
+                date = "No record"
+            return render(request, "contacts.html", {
+                "venues": ["dllm", "on99"],
+                "subject": user,
+                "date": date,
+            })
+        else:
+            return render(request, "base.html", {
+                "subject": "No input",
+                "date": "No input",
+            })
